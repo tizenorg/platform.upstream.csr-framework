5adc94c8c52f68d98ab0c78f58ed8bdcbfc69681
Kyungwook Tak <k.tak@samsung.com>
2016-04-26 21:14:04 +0900

    Type converter for capi <-> engine interface
    Change-Id: I266fbcf1d4ad98db6fc4a054f4e0827f3d823dd1
    Signed-off-by: Kyungwook Tak <k.tak@samsung.com>

---
+++ src/framework/service/type-converter.cpp
@@ -0,0 +1,84 @@
+/*
+ *  Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        type-converter.cpp
+ * @author      Kyungwook Tak (k.tak@samsung.com)
+ * @version     1.0
+ * @brief       Converts types of engine interface to capi interface
+ */
+#include "service/type-converter.h"
+
+#include <stdexcept>
+
+#include "common/audit/logger.h"
+
+namespace Csr {
+
+csr_cs_severity_level_e convert(const csre_cs_severity_level_e &e)
+{
+	switch (e) {
+	case CSRE_CS_SEVERITY_LOW:
+		return CSR_CS_SEVERITY_LOW;
+
+	case CSRE_CS_SEVERITY_MEDIUM:
+		return CSR_CS_SEVERITY_MEDIUM;
+
+	case CSRE_CS_SEVERITY_HIGH:
+		return CSR_CS_SEVERITY_HIGH;
+
+	default:
+		throw std::logic_error(FORMAT("Invalid eseverity: " << static_cast<int>(e)));
+	}
+}
+
+csr_cs_threat_type_e convert(const csre_cs_threat_type_e &e)
+{
+	switch (e) {
+	case CSRE_CS_THREAT_MALWARE:
+		return CSR_CS_THREAT_MALWARE;
+
+	case CSRE_CS_THREAT_RISKY:
+		return CSR_CS_THREAT_RISKY;
+
+	case CSRE_CS_THREAT_GENERIC:
+		return CSR_CS_THREAT_GENERIC;
+
+	default:
+		throw std::logic_error(FORMAT("Invalid ethreat: " << static_cast<int>(e)));
+	}
+}
+
+csr_wp_risk_level_e convert(const csre_wp_risk_level_e &e)
+{
+	switch (e) {
+	case CSRE_WP_RISK_LOW:
+		return CSR_WP_RISK_LOW;
+
+	case CSRE_WP_RISK_UNVERIFIED:
+		return CSR_WP_RISK_UNVERIFIED;
+
+	case CSRE_WP_RISK_MEDIUM:
+		return CSR_WP_RISK_MEDIUM;
+
+	case CSRE_WP_RISK_HIGH:
+		return CSR_WP_RISK_HIGH;
+
+	default:
+		throw std::logic_error(FORMAT("Invalid elevel: " << static_cast<int>(e)));
+	}
+}
+
+}
---
+++ src/framework/service/type-converter.h
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file        type-converter.h
+ * @author      Kyungwook Tak (k.tak@samsung.com)
+ * @version     1.0
+ * @brief       Converts types of engine interface to capi interface
+ */
+#pragma once
+
+#include "csre/content-screening-types.h"
+#include "csr/content-screening-types.h"
+#include "csre/web-protection-types.h"
+#include "csr/web-protection-types.h"
+
+namespace Csr {
+
+csr_cs_severity_level_e convert(const csre_cs_severity_level_e &);
+csr_cs_threat_type_e convert(const csre_cs_threat_type_e &);
+
+csr_wp_risk_level_e convert(const csre_wp_risk_level_e &);
+
+}

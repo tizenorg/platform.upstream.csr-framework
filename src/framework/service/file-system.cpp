cba163affb769478ed48850de808ee1008d843b3
Kyungwook Tak <k.tak@samsung.com>
2016-05-18 22:56:56 +0900

    Determine file changed by changed time
    For modified time (st_mtime field in struct stat), it isn't updated on
    tpk package in same cases. The modified time is fixed on when it
    packaged, so use changed time instead.
    
    Change-Id: Id8ea85ebca8a8c8b5fec224b936c538c44807592
    Signed-off-by: Kyungwook Tak <k.tak@samsung.com>

--- src/framework/service/file-system.cpp
+++ src/framework/service/file-system.cpp
@@ -165,8 +165,8 @@ FilePtr File::create(const std::string &fpath, time_t modifiedSince)
 		ThrowExc(FileDoNotExist, "file not exist: " << fpath);
 	} else if (!S_ISREG(statptr->st_mode) && !S_ISDIR(statptr->st_mode)) {
 		ThrowExc(FileSystemError, "file type is not reguler or dir: " << fpath);
-	} else if (modifiedSince != -1 && statptr->st_mtim.tv_sec <= modifiedSince) {
-		DEBUG("file[" << fpath << "] isn't modified since[" << modifiedSince << "]");
+	} else if (modifiedSince != -1 && statptr->st_ctime <= modifiedSince) {
+		DEBUG("file[" << fpath << "] isn't changed since[" << modifiedSince << "]");
 		return nullptr;
 	} else {
 		return FilePtr(new File(fpath, S_ISDIR(statptr->st_mode)));

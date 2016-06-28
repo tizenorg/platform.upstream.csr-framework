6d0db05520788fd911d721686d9d665fc9ea66de
sangwan.kwon <sangwan.kwon@samsung.com>
2016-06-28 16:57:16 +0900

    Refactor scanAppDelta on cs-logic
    Change-Id: Iacbc2504b763e754df8b6cff666e4677089a03bf
    Signed-off-by: sangwan.kwon <sangwan.kwon@samsung.com>

--- src/framework/service/cs-logic.h
+++ src/framework/service/cs-logic.h
@@ -16,6 +16,7 @@
 /*
  * @file        cs-logic.h
  * @author      Kyungwook Tak (k.tak@samsung.com)
+ * @author      Sangwan Kwon (sangwan.kwon@samsung.com)
  * @version     1.0
  * @brief
  */
@@ -70,10 +71,29 @@ private:
 	RawBuffer handleAskUser(const CsContext &c, CsDetected &d,
 							FilePtr &&fileptr = nullptr);
 
+	Db::RowShPtr getWorseByPkgPath(const std::string &pkgPath, time_t since);
+
+	enum class ScanStage : int {
+		NEW_RISKIEST      = 0x1001,
+		HISTORY_RISKIEST  = 0x1002,
+		WORSE_RISKIEST    = 0x1003,
+		NO_DETECTED       = 0x1004
+	};
+
+	CsLogic::ScanStage judgeScanStage(
+			const Db::RowShPtr &history,
+			const Db::RowShPtr &after,
+			const CsDetectedPtr &riskiest,
+			Db::RowShPtr &jWorse,
+			Db::RowShPtr &jHistory,
+			const std::string &pkgPath,
+			time_t since);
+
 	std::shared_ptr<CsLoader> m_loader;
 	std::shared_ptr<Db::Manager> m_db;
 
 	std::string m_dataVersion;
+
 };
 
 }
